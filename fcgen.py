cc00d35ad65a911054d6b2d32fe1b0fafdbd4b54
kiki <makarewicz.krystian@gmail.com>
2024-02-16 14:53:53 +0100

    The Application class has been further refactored and simplified and docstring comments have been added

--- fcgen.py
+++ fcgen.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import time
 import json
 import stdiomask
 
@@ -8,12 +7,17 @@ from data import *
 
 
 class Application:
+    """ Main class of the application. It is responsible for managing the flow of the application. """
+
     def __init__(self):
+        """ Initialize the application. """
         self.current_module = None
         self.current_stage = None
         self.current_profile = None
         self.profile_manager = ProfileManager()
-        self.determine_current_stage()
+        self.stage_and_module_handler = StageAndModuleHandler(self)
+        self.user_input_handler = UserInputHandler(self)
+        self.stage_and_module_handler.determine_current_stage()
         self.menu = Menu(self.current_module, self.current_stage)
         self.action_dispatcher = {
             'new_profile': AddNewProfile(self, self.profile_manager),
@@ -29,6 +33,7 @@ class Application:
         }
 
     def main(self):
+        """ Main loop for the application. """
         Tools.clear_screen()
         while True:
             self.profile_manager.display_profile_info()
@@ -36,68 +41,100 @@ class Application:
             self.menu.update(self.current_module, self.current_stage)
             self.menu.display_menu()
             user_input = input('>>>> ')
-            self.handle_user_input(user_input)
+            self.user_input_handler.handle_user_input(user_input)
+
+
+class StageAndModuleHandler:
+    """ Class responsible for managing the current stage and module of the application. """
+
+    def __init__(self, app_inst):
+        """ Initialize the handler. """
+        self.app_inst = app_inst
 
     def determine_current_stage(self):
-        if not self.profile_manager.profiles:
+        """ Determine the current stage of the application. """
+        if not self.app_inst.profile_manager.profiles:
             self.set_current_stage('profile', 'initiation')
-        elif not self.current_profile:
+        elif not self.app_inst.current_profile:
             self.set_current_stage('profile', 'no_profile_selected')
 
     def set_current_stage(self, module, stage):
-        self.current_module = module
-        self.current_stage = stage
+        """ Set the current stage of the application. """
+        self.app_inst.current_module = module
+        self.app_inst.current_stage = stage
 
     def change_stage(self, new_stage):
+        """ Change the current stage of the application. """
         if new_stage in stages:
-            self.current_stage = new_stage
-            self.menu = Menu(self.current_module, self.current_stage)
+            self.app_inst.current_stage = new_stage
+            self.app_inst.menu = Menu(self.app_inst.current_module, self.app_inst.current_stage)
         else:
             print(f'Invalid stage: {new_stage}.')
+            input('Press Enter to continue...')
+            Tools.clear_screen()
+
+
+class UserInputHandler:
+    """ Class responsible for handling user input. """
+
+    def __init__(self, app_inst):
+        """ Initialize the handler. """
+        self.app_inst = app_inst
 
     def handle_user_input(self, user_input):
+        """ Handle user input. """
         input_to_action_key = self.input_to_action_key()
         user_action_key = next((k for k, v in input_to_action_key.items() if k.startswith(user_input)), None)
         action_key = input_to_action_key.get(user_action_key)
         if action_key:
-            action = self.action_dispatcher.get(action_key)
+            action = self.app_inst.action_dispatcher.get(action_key)
             action.execute()
         else:
             print(f'Option {user_input} is not available.')
-            time.sleep(4)
+            input('Press Enter to continue...')
             Tools.clear_screen()
 
     def input_to_action_key(self):
-        menu_items_dict = module_menus[self.current_module]
+        """ Convert user input to action key. """
+        menu_items_dict = module_menus[self.app_inst.current_module]
         return {v: k for k, v in menu_items_dict.items()}
 
 
 class Tools:
+    """ Class with tools for the application. """
     clear_command = 'cls' if os.name == 'nt' else 'clear'
 
     @staticmethod
     def clear_screen():
+        """ Clear the screen. """
         os.system(Tools.clear_command)
 
 
 class Menu:
+    """ Class responsible for managing the menu of the application. """
+
     def __init__(self, current_module: str, current_stage: str):
+        """ Initialize the menu. """
         self.current_module = current_module
         self.current_stage = current_stage
         self.items = self.get_menu_items()
 
     def get_menu_items(self):
+        """ Get menu items for the current stage and module. """
         menu_items_dict = module_menus[self.current_module]
         return [menu_items_dict[item_id] for item_id in stages[self.current_stage]]
 
     def display_menu(self):
+        """ Display the menu. """
         for item in self.items:
             print(item)
 
     def needs_update(self, new_module, new_stage):
+        """ Check if the menu needs to be updated. """
         return new_module != self.current_module or new_stage != self.current_stage
 
     def update(self, new_module, new_stage):
+        """ Update the menu. """
         if self.needs_update(new_module, new_stage):
             self.current_module = new_module
             self.current_stage = new_stage
@@ -105,16 +142,21 @@ class Menu:
 
 
 class Profile:
+    """ Class representing a profile. """
+
     def __init__(self, profile_name=None, openai_api_key=None, gpt_model=None):
+        """ Initialize the profile. """
         self.profile_name = profile_name
         self.openai_api_key = openai_api_key
         self.gpt_model = gpt_model
 
     def __str__(self):
+        """ Return a string representation of the profile. """
         return (f'{self.profile_name} (OpenAI API Key: sk-...{self.openai_api_key[-4:]}, GPT Model: '
                 f'{self.gpt_model})')
 
     def as_dict(self):
+        """ Return the profile as a dictionary. """
         return {
             'profile_name': self.profile_name,
             'openai_api_key': self.openai_api_key,
@@ -122,6 +164,7 @@ class Profile:
         }
 
     def save_to_file(self, file_path):
+        """ Save the profile to a file. """
         directory = os.path.dirname(file_path)
         if not os.path.isdir(directory):
             os.makedirs(directory, exist_ok=True)
@@ -130,12 +173,16 @@ class Profile:
 
 
 class ProfileManager:
+    """ Class responsible for managing profiles. """
+
     def __init__(self, profiles_dir='profiles'):
+        """ Initialize the profile manager. """
         self.profiles_dir = profiles_dir
         self.profiles = self.load_profiles()
         self.profile = self.determine_current_profile()
 
     def load_profiles(self):
+        """ Load profiles from files. """
         if not os.path.exists(self.profiles_dir):
             os.makedirs(self.profiles_dir, exist_ok=True)
             return []
@@ -158,6 +205,8 @@ class ProfileManager:
         return profiles
 
     def select_profile(self):
+        """ Select a profile from the list of profiles. """
+        self.profiles = self.load_profiles()
         for index, profile in enumerate(self.profiles):
             print(f'{index + 1}. {profile}')
         while True:
@@ -171,12 +220,14 @@ class ProfileManager:
 
     @staticmethod
     def validate_profile_files(data):
+        """ Validate if file is a valid profile file. """
         requires_fields = ['profile_name', 'openai_api_key', 'gpt_model']
         if not all(field in data for field in requires_fields):
             return False
         return True
 
     def validate_profile_data(self, profile):
+        """ Validate profile data and update it if necessary. """
         profile_name = profile.profile_name
         openai_api_key = profile.openai_api_key
         gpt_model = profile.gpt_model
@@ -197,6 +248,7 @@ class ProfileManager:
             print(f'Profile "{profile.profile_name}" updated and saved.')
 
     def add_new_profile(self):
+        """ Add a new profile. """
         while True:
             profile_name = input('Enter your new profile name: ')
             if self.is_unique_profile_name(profile_name):
@@ -209,6 +261,7 @@ class ProfileManager:
         new_profile.save_to_file(f'profiles/{profile_name}.json')
 
     def edit_profile(self):
+        """ Edit a profile from the list of profiles."""
         # TODO: After selecting a profile and then editing it, the data displayed in the profile information remains unchanged.
         selected_profile = self.select_profile()
         profile_dict = selected_profile.as_dict()
@@ -241,18 +294,22 @@ class ProfileManager:
                 print('Invalid choice. Please select 1-3 to edit selected element or 9 to finish editing.')
 
         self.update_profile(original_profile_name, profile_dict)
+        self.profile = self.determine_current_profile()
 
     @staticmethod
     def display_edit_menu(profile_dict):
+        """ Display the edit menu for the profile. """
         print('What you want to edit: ')
         for index, (key, _) in enumerate(profile_dict.items(), start=1):
             print(f'{index}. {key.replace("_", " ").upper()}')
         print('9. Finish editing')
 
     def is_unique_profile_name(self, profile_name):
+        """ Check if the profile name is unique. """
         return not any(profile.profile_name == profile_name for profile in self.profiles)
 
     def update_profile(self, original_profile_name, profile_dict):
+        """ Update the profile with new data. """
         new_profile = Profile(**profile_dict)
         new_profile.save_to_file(os.path.join('profiles', f'{new_profile.profile_name}.json'))
         if original_profile_name != new_profile.profile_name:
@@ -263,6 +320,7 @@ class ProfileManager:
 
     @staticmethod
     def get_openai_api_key_from_user():
+        """ Get OpenAI API key from the user. """
         while True:
             openai_api_key = stdiomask.getpass(prompt='Enter your OpenAI API key: ')
             if (len(openai_api_key) == 51
@@ -274,6 +332,7 @@ class ProfileManager:
 
     @staticmethod
     def select_model():
+        """ Select GPT model from the list of available models."""
         while True:
             print('Select GPT model:')
             for index, model in enumerate(openai_models):
@@ -285,86 +344,109 @@ class ProfileManager:
                 print(f'Invalid choice. Please enter a number between 0 and {len(openai_models) - 1}.')
 
     def save_current_profile(self):
+        """ Save the current profile information to env.json file. """
         pass
 
-    def determine_current_profile(self):
-        return None
-
     def display_profile_info(self):
+        """ Display the current profile information. """
         if not self.profile:
             print(f'>>>>> Current profile: NO PROFILE SELECTED <<<<<')
         else:
             print(f'>>>>> Current profile: {self.profile}] <<<<<')
 
+    def determine_current_profile(self):
+        return None
+
 
 class Action:
+    """ Class representing an action. """
+
     def __init__(self, app_inst):
+        """ Initialize the action. """
         self.app_inst = app_inst
 
     def execute(self):
+        """ Execute the action """
         raise NotImplementedError("Subclasses should implement this!")
 
     def log(self, message):
+        """ Log a message. """
         print(f'[LOG] {message}')
 
 
 class AddNewProfile(Action):
+    """ Class representing an action of adding a new profile. """
+
     def __init__(self, app_inst, profile_manager):
+        """ Initialize the action. """
         super().__init__(app_inst)
         self.profile_manager = profile_manager
 
     def execute(self):
+        """ Execute the action """
         Tools.clear_screen()
         self.log('Adding a new profile...')
 
         self.profile_manager.add_new_profile()
 
         print('New profile added.')
-        time.sleep(3)
+        input('Press Enter to continue...')
         Tools.clear_screen()
         profiles = self.profile_manager.load_profiles()
         if profiles and not self.profile_manager.profile:
-            self.app_inst.change_stage('no_profile_selected')
+            self.app_inst.stage_and_module_handler.change_stage('no_profile_selected')
 
 
 class SelectProfile(Action):
+    """ Class representing an action of selecting a profile. """
+
     def __init__(self, app_inst, profile_manager):
+        """ Initialize the action. """
         super().__init__(app_inst)
         self.profile_manager = profile_manager
 
     def execute(self):
+        """ Execute the action """
         Tools.clear_screen()
         self.log('Profile selection...')
 
         profile = self.profile_manager.select_profile()
 
         print(f'Profile: {profile} selected.')
-        time.sleep(3)
+        input('Press Enter to continue...')
         Tools.clear_screen()
-        self.app_inst.change_stage('profile_selected')
+        self.app_inst.stage_and_module_handler.change_stage('profile_selected')
 
 
 class EditProfile(Action):
+    """ Class representing an action of editing a profile. """
+
     def __init__(self, app_inst, profile_manager):
+        """ Initialize the action. """
         super().__init__(app_inst)
         self.profile_manager = profile_manager
 
     def execute(self):
+        """ Execute the action """
         Tools.clear_screen()
         self.log('Profile editing...')
         print('Which profile you want to edit?')
         self.profile_manager.edit_profile()
         print('Profile edited.')
-        time.sleep(7)
+        input('Press Enter to continue...')
         Tools.clear_screen()
 
 
 class ExitProgram(Action):
+    """ Class representing an action of exiting the program. """
+
     def __init__(self, app_inst, profile_manager):
+        """ Initialize the action. """
         super().__init__(app_inst)
         self.profile_manager = profile_manager
 
     def execute(self):
+        """ Execute the action """
         Tools.clear_screen()
         while True:
             answer = input('Are you sure you want to close this application? (Y/N) ')
@@ -376,11 +458,16 @@ class ExitProgram(Action):
                 break
             else:
                 print('Invalid answer, select "Y" or "N".')
-                time.sleep(3)
+                input('Press Enter to continue...')
                 Tools.clear_screen()
                 pass
 
 
-if __name__ == "__main__":
+def run_application():
+    """ Create an application instance and run it. """
     app = Application()
     app.main()
+
+
+if __name__ == "__main__":
+    run_application()
