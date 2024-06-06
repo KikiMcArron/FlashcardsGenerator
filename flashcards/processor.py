85c7ecc7e5250308c016d6234fb6685ecb166585
Kiki McArron <makarewicz.krystian@gmail.com>
2024-06-06 17:18:27 +0200

    I've added DtaclassEditor class in processor.py module, which allows to edit dataclasses in a text editor

---
+++ flashcards/processor.py
@@ -0,0 +1,72 @@
+from dataclasses import is_dataclass, fields
+from typing import TypeVar, Generic, Optional, List
+import os
+import subprocess
+import tempfile
+
+
+T = TypeVar('T')
+
+
+class DataclassEditor(Generic[T]):
+    """Class for editing dataclasses in a text editor."""
+    def __init__(self, editor: Optional[str] = None, display_fields: Optional[List[str]] = None) -> None:
+        """
+        Initialize the DataclassEditor.
+
+        :param editor: The text editor to use. If None, the default editor will be used.
+        :param display_fields: The fields to display in the editor. If None, all fields will be displayed.
+        """
+        self.editor = editor if editor else os.getenv('EDITOR', 'nano')
+        self.display_fields = display_fields
+
+    def _write_dataclass_to_tempfile(self, obj: T) -> str:
+        """
+        Write the dataclass to a temporary file.
+
+        :param obj: The dataclass object to write to the file.
+        :return: The path to the temporary file.
+        """
+        if not is_dataclass(obj):
+            raise ValueError('The provided object is not a dataclass.')
+
+        tmpfile = tempfile.NamedTemporaryFile(delete=False, mode='w+')
+        try:
+            for field in fields(obj):
+                if self.display_fields is None or field.name in self.display_fields:
+                    value = getattr(obj, field.name)
+                    tmpfile.write(f'{field.name.capitalize()}: {value}\n')
+            tmpfile.flush()
+            return tmpfile.name
+        finally:
+            tmpfile.close()
+
+    @staticmethod
+    def _read_dataclass_from_tempfile(filepath: str, obj_type: type) -> T:
+        """
+        Read the dataclass from a temporary file.
+
+        :param filepath: The path to the temporary file.
+        :param obj_type: The type of the dataclass object.
+        :return: The dataclass object read from the file.
+        """
+        with open(filepath, 'r') as tmpfile:
+            data = {}
+            for line in tmpfile:
+                name, value = line.strip().split(': ', 1)
+                field_name = name.lower()
+                data[field_name] = value
+        os.remove(filepath)
+        return obj_type(**data)
+
+    def edit_dataclass(self, obj: T) -> T:
+        """
+        Edit the dataclass object in a text editor.
+
+        :param obj: The dataclass object to edit.
+        :return: The modified dataclass object.
+        """
+        obj_file = type(obj)
+        filepath = self._write_dataclass_to_tempfile(obj)
+        subprocess.run([self.editor, filepath])
+        return self._read_dataclass_from_tempfile(filepath, obj_file)
