e09d42e8051ed6385dd3444e3ebf6cacb279fb1a
Kiki McArron <makarewicz.krystian@gmail.com>
2024-09-11 00:33:41 +0200

    I copied all menu elements and stageges lists from first version of my app to ui/menu/py and configurations values to settings.py and I created ui_manager.py in ui package

---
+++ settings.py
@@ -0,0 +1,11 @@
+SETTINGS_FILE = 'settings.json'
+
+PROFILES_DIR = 'profiles'
+
+file_types = [
+    ('Text files', '.txt'),
+    ('PDF files', '.pdf'),
+    ('All files', '.*')
+]
+
+openai_models = ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-turbo-preview']
