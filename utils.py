5108c3f47d29dd02571d8614fe6701ef037c4608
Kiki McArron <makarewicz.krystian@gmail.com>
2024-10-07 14:52:59 +0200

    Remove docstrings from utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import json
 import os
 import platform
-# from typing import Dict, List, Optional
 
 import bcrypt
 
@@ -9,16 +8,11 @@ clear_command = 'cls' if os.name == 'nt' else 'clear'
 
 
 def clear_screen() -> None:
-    """ Clear the screen. """
     os.system(clear_command)
 
 
 def get_default_text_editor() -> str:
-    """
-    Get the default text editor.
 
-    :return: The default text editor.
-    """
     system = platform.system()
     if system == 'Windows':
         return 'notepad'
@@ -37,9 +31,8 @@ def _encrypt(secure_txt: str) -> str:
 
 
 def save_to_file(data_dict, file_path) -> None:
-    """ Save the profile to a file. """
     directory = os.path.dirname(file_path)
-    _ensure_dir_exists(directory)
+    ensure_dir_exists(directory)
     with open(file_path, 'w') as file:
         if isinstance(data_dict, dict):
             json.dump(data_dict, file, indent=4)
@@ -47,7 +40,6 @@ def save_to_file(data_dict, file_path) -> None:
             file.write(data_dict)
 
 
-def _ensure_dir_exists(directory) -> None:
-    """ Ensure that the directory exists. """
+def ensure_dir_exists(directory) -> None:
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
